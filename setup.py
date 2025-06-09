a5651a10b4134cc4a21a06926a4f93043b063e4f
urasakikeisuke <79469739+urasakikeisuke@users.noreply.github.com>
2025-06-09 17:00:49 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,228 +1,141 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Note: To use the 'upload' functionality of this file, you must:
-#   $ pip install twine
-
 import io
 import os
-import shutil
+import re
 import sys
 from pathlib import Path
-from shutil import rmtree
 from typing import List
 
 import pccm
-from pccm.extension import ExtCallback, PCCMBuild, PCCMExtension
-from setuptools import Command, find_packages, setup
-from setuptools.extension import Extension
 from ccimport import compat
-import subprocess 
-import re 
+from pccm.extension import PCCMBuild, PCCMExtension
+from setuptools import find_packages, setup
+from setuptools.extension import Extension
 
-# Package meta-data.
+# 使用するCUDAのバージョン（例: "11.8"）。
+CUMM_CUDA_VERSION = os.environ.get("CUMM_CUDA_VERSION", "")
+
+# --- パッケージのメタデータ定義 ---
 NAME = 'spconv'
 RELEASE_NAME = NAME
 deps = ["cumm"]
-cuda_ver = os.environ.get("CUMM_CUDA_VERSION", "")
 
-# is_ci_build = cuda_ver != ""
-# if not cuda_ver:
-#     nvcc_version = subprocess.check_output(["nvcc", "--version"
-#                                             ]).decode("utf-8").strip()
-#     nvcc_version_str = nvcc_version.split("\n")[3]
-#     version_str: str = re.findall(r"release (\d+.\d+)",
-#                                     nvcc_version_str)[0]
-#     cuda_ver = version_str
-
-if cuda_ver:
-    cuda_ver_str = cuda_ver.replace(".", "") # 10.2 to 102
-
-    RELEASE_NAME += "-cu{}".format(cuda_ver_str)
-    deps = ["cumm-cu{}>=0.7.11, <0.8.0".format(cuda_ver_str)]
+# CUDAバージョンに基づいてリリース名と依存関係を調整
+if CUMM_CUDA_VERSION:
+    cuda_ver_str = CUMM_CUDA_VERSION.replace(".", "")  # 11.8 -> 118
+    RELEASE_NAME += f"-cu{cuda_ver_str}"
+    deps = [f"cumm-cu{cuda_ver_str}>=0.7.11, <1.0.0"]
 else:
-    deps = ["cumm>=0.7.11, <0.8.0"]
-
-
+    # CUMM_CUDA_VERSIONが設定されていない場合はビルドを中止
+    raise RuntimeError("CUMM_CUDA_VERSION must be set. e.g. export CUMM_CUDA_VERSION=11.8")
 
 DESCRIPTION = 'spatial sparse convolution'
 URL = 'https://github.com/traveller59/spconv'
 EMAIL = 'yanyan.sub@outlook.com'
 AUTHOR = 'Yan Yan'
 REQUIRES_PYTHON = '>=3.9'
-VERSION = None
 
-# What packages are required for this module to be executed?
-REQUIRED = ["pccm>=0.4.16", "ccimport>=0.4.4", "pybind11>=2.6.0", "fire", "numpy", *deps]
+# --- 依存パッケージの定義 ---
+REQUIRED = [
+    "pccm>=0.4.16",
+    "ccimport>=0.4.4",
+    "pybind11>=2.6.0",
+    "fire",
+    "numpy<2.0.0",
+    *deps
+]
 
-# What packages are optional?
-EXTRAS = {
-    # 'fancy feature': ['django'],
-}
-
-# The rest you shouldn't have to touch too much :)
-# ------------------------------------------------
-# Except, perhaps the License and Trove Classifiers!
-# If you do change the License, remember to change the Trove Classifier for that!
+EXTRAS = {}
 
 here = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(str(Path(__file__).parent))
 
-# Import the README and use it as the long-description.
-# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
 try:
     with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
         long_description = '\n' + f.read()
 except FileNotFoundError:
     long_description = DESCRIPTION
 
-# Load the package's __version__.py module as a dictionary.
+# バージョン情報の読み込みと設定
 about = {}
-if not VERSION:
-    with open('version.txt', 'r') as f:
-        version = f.read().strip()
-else:
-    version = VERSION
-cwd = os.path.dirname(os.path.abspath(__file__))
+with open('version.txt', 'r') as f:
+    version = f.read().strip()
 
-
-def _convert_build_number(build_number):
-    parts = build_number.split(".")
-    if len(parts) == 2:
-        return "{}{:03d}".format(int(parts[0]), int(parts[1]))
-    elif len(parts) == 1:
-        return build_number
-    else:
-        raise NotImplementedError
-
-
-env_suffix = os.environ.get("SPCONV_VERSION_SUFFIX", "")
-if env_suffix != "":
-    version += ".dev{}".format(_convert_build_number(env_suffix))
-version_path = os.path.join(cwd, NAME, '__version__.py')
+version_path = Path(here) / NAME / '__version__.py'
+with open(version_path, 'w') as f:
+    f.write(f"__version__ = '{version}'\n")
 about['__version__'] = version
 
-with open(version_path, 'w') as f:
-    f.write("__version__ = '{}'\n".format(version))
+# --- AOT (事前コンパイル) の設定 ---
 
-class UploadCommand(Command):
-    """Support setup.py upload."""
+# setup.py build_ext 実行時に pccm のビルドプロセスを呼び出すように設定
+cmdclass = {
+    'build_ext': PCCMBuild,
+}
 
-    description = 'Build and publish the package.'
-    user_options = []
+# C++拡張のビルドに必要なモジュールをインポート
+from cumm.common import CompileInfo
+from cumm.conv.main import ConvMainUnitTest
+from cumm.gemm.main import GemmMainUnitTest
+from spconv.core import (IMPLGEMM_AMPERE_PARAMS, IMPLGEMM_SIMT_PARAMS,
+                         IMPLGEMM_TURING_PARAMS, IMPLGEMM_VOLTA_PARAMS,
+                         SHUFFLE_AMPERE_PARAMS, SHUFFLE_SIMT_PARAMS,
+                         SHUFFLE_TURING_PARAMS, SHUFFLE_VOLTA_PARAMS)
+from spconv.csrc.hash.core import HashTable
+from spconv.csrc.sparse.all import SpconvOps
+from spconv.csrc.sparse.alloc import ExternalAllocator
+from spconv.csrc.sparse.convops import (ConvGemmOps, ConvTunerSimple,
+                                        ExternalSpconvMatmul, GemmTunerSimple)
+from spconv.csrc.sparse.inference import InferenceOps
+from spconv.csrc.utils import BoxOps, PointCloudCompress
 
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
+# ビルド対象のカーネルパラメータをすべて結合
+all_shuffle = SHUFFLE_SIMT_PARAMS + SHUFFLE_VOLTA_PARAMS + SHUFFLE_TURING_PARAMS + SHUFFLE_AMPERE_PARAMS
+all_imp = (IMPLGEMM_SIMT_PARAMS + IMPLGEMM_VOLTA_PARAMS +
+           IMPLGEMM_TURING_PARAMS + IMPLGEMM_AMPERE_PARAMS)
 
-    def initialize_options(self):
-        pass
+cu = GemmMainUnitTest(all_shuffle)
+cu.namespace = "cumm.gemm.main"
+convcu = ConvMainUnitTest(all_imp)
+convcu.namespace = "cumm.conv.main"
 
-    def finalize_options(self):
-        pass
+# CUDAバージョンに基づいて使用するC++標準を決定
+std = "c++17"
+cuda_ver_tuple = tuple(map(int, CUMM_CUDA_VERSION.split(".")))
+if cuda_ver_tuple[0] < 11:
+    std = "c++14"
 
-    def run(self):
-        try:
-            self.status('Removing previous builds...')
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
+# pccmに渡すC++コード定義体のリストを作成
+gemmtuner = GemmTunerSimple(cu)
+gemmtuner.namespace = "csrc.sparse.convops.gemmops"
+convtuner = ConvTunerSimple(convcu)
+convtuner.namespace = "csrc.sparse.convops.convops"
+convops = ConvGemmOps(gemmtuner, convtuner)
+convops.namespace = "csrc.sparse.convops.spops"
 
-        self.status('Building Source and Wheel (universal) distribution...')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(
-            sys.executable))
+cus = [
+    gemmtuner, convtuner, convops, SpconvOps(), BoxOps(), HashTable(),
+    CompileInfo(), ExternalAllocator(), PointCloudCompress(),
+    ExternalSpconvMatmul(), InferenceOps()
+]
+# GPU関連のコアコンポーネントを追加
+cus.extend([cu, convcu])
 
-        self.status('Uploading the package to PyPI via Twine...')
-        os.system('twine upload dist/*')
+# setuptoolsに渡す拡張モジュールを定義
+ext_modules: List[Extension] = [
+    PCCMExtension(
+        cus,
+        "spconv/core_cc",  # 生成されるモジュール名
+        Path(__file__).resolve().parent / "spconv",  # ソース生成先
+        std=std,
+        disable_pch=True,
+        verbose=True
+    )
+]
 
-        self.status('Pushing git tags...')
-        os.system('git tag v{0}'.format(about['__version__']))
-        os.system('git push --tags')
-
-        sys.exit()
-
-
-
-disable_jit = os.getenv("SPCONV_DISABLE_JIT", None)
-
-if disable_jit is not None and disable_jit == "1":
-    cmdclass = {
-        'upload': UploadCommand,
-        'build_ext': PCCMBuild,
-    }
-    from cumm.gemm.main import GemmMainUnitTest
-    from spconv.core import SHUFFLE_SIMT_PARAMS, SHUFFLE_VOLTA_PARAMS, SHUFFLE_TURING_PARAMS, SHUFFLE_AMPERE_PARAMS
-    from spconv.core import IMPLGEMM_SIMT_PARAMS, IMPLGEMM_VOLTA_PARAMS, IMPLGEMM_TURING_PARAMS, IMPLGEMM_AMPERE_PARAMS
-    from cumm.conv.main import ConvMainUnitTest
-    from cumm.constants import CUMM_CPU_ONLY_BUILD
-    from spconv.csrc.sparse.all import SpconvOps
-    from spconv.csrc.utils import BoxOps, PointCloudCompress
-    from spconv.csrc.hash.core import HashTable
-    from cumm.common import CompileInfo
-    from spconv.csrc.sparse.alloc import ExternalAllocator
-    from spconv.csrc.sparse.convops import GemmTunerSimple, ExternalSpconvMatmul
-    from spconv.csrc.sparse.convops import ConvTunerSimple, ConvGemmOps
-    from spconv.csrc.sparse.inference import InferenceOps
-    all_shuffle = SHUFFLE_SIMT_PARAMS + SHUFFLE_VOLTA_PARAMS + SHUFFLE_TURING_PARAMS + SHUFFLE_AMPERE_PARAMS
-    all_imp = (IMPLGEMM_SIMT_PARAMS + IMPLGEMM_VOLTA_PARAMS +
-               IMPLGEMM_TURING_PARAMS + IMPLGEMM_AMPERE_PARAMS)
-    # all_shuffle = list(filter(lambda x: not x.is_nvrtc, all_shuffle))
-    # all_imp = list(filter(lambda x: not x.is_nvrtc, all_imp))
-
-    cu = GemmMainUnitTest(all_shuffle)
-    convcu = ConvMainUnitTest(all_imp)
-    convcu.namespace = "cumm.conv.main"
-
-    cu.namespace = "cumm.gemm.main"
-    std = "c++17"
-    if cuda_ver:
-        cuda_ver_items = cuda_ver.split(".")
-        if len(cuda_ver_items) == 1:
-            cuda_ver_num = int(cuda_ver)
-            cuda_ver_tuple = (cuda_ver_num // 10, cuda_ver_num % 10)
-        else:
-            cuda_ver_vec = list(map(int, cuda_ver.split(".")))
-            cuda_ver_tuple = (cuda_ver_vec[0], cuda_ver_vec[1])
-        if cuda_ver_tuple[0] < 11:
-            std = "c++14" 
-        else:
-            std = "c++17"
-    if not CUMM_CPU_ONLY_BUILD:
-        gemmtuner = GemmTunerSimple(cu)
-        gemmtuner.namespace = "csrc.sparse.convops.gemmops"
-        convtuner = ConvTunerSimple(convcu)
-        convtuner.namespace = "csrc.sparse.convops.convops"
-        convops = ConvGemmOps(gemmtuner, convtuner)
-        convops.namespace = "csrc.sparse.convops.spops"
-    else:
-        gemmtuner = GemmTunerSimple(None)
-        gemmtuner.namespace = "csrc.sparse.convops.gemmops"
-        convtuner = ConvTunerSimple(None)
-        convtuner.namespace = "csrc.sparse.convops.convops"
-        convops = ConvGemmOps(gemmtuner, convtuner)
-        convops.namespace = "csrc.sparse.convops.spops"
-    cus = [gemmtuner, convtuner,
-        convops, SpconvOps(), BoxOps(), HashTable(), CompileInfo(), 
-        ExternalAllocator(), PointCloudCompress(),
-        ExternalSpconvMatmul(), InferenceOps()]
-    if not CUMM_CPU_ONLY_BUILD:
-        cus.extend([cu, convcu])
-    ext_modules: List[Extension] = [
-        PCCMExtension(cus,
-                      "spconv/core_cc",
-                      Path(__file__).resolve().parent / "spconv",
-                      std=std,
-                      disable_pch=True,
-                      verbose=True)
-    ]
-else:
-    cmdclass = {
-        'upload': UploadCommand,
-    }
-    ext_modules = []
-
-# Where the magic happens:
+# --- パッケージング実行 ---
 setup(
     name=RELEASE_NAME,
     version=about['__version__'],
@@ -233,19 +146,12 @@ setup(
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    packages=find_packages(exclude=('tests', )),
-    # If your package is a single module, use this instead of 'packages':
-    # py_modules=['mypackage'],
-    entry_points={
-        'console_scripts': [],
-    },
+    packages=find_packages(exclude=('tests',)),
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     include_package_data=True,
     license='Apache License 2.0',
     classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
@@ -256,7 +162,6 @@ setup(
         'Programming Language :: Python :: 3.13',
         'Programming Language :: Python :: Implementation :: CPython',
     ],
-    # $ setup.py publish support.
     cmdclass=cmdclass,
     ext_modules=ext_modules,
 )
